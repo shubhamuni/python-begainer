f5bde9b3f7961c1a3ee334106680d8e21938daa0
shubhampatil01 <89505897+shubhampatil01@users.noreply.github.com>
2022-09-08 22:20:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-print('Shubham')
-hjfbgdsg
+i = 1
+
+    while i <= n:
+        if i % 2 == 0:
+            print(i)
+        i = i + 1
+   
+    
+    
+    
+    
+    
+    
+    
+   
+def main():
+    n=int(input())
+    print_even(n)
+    
+main()
